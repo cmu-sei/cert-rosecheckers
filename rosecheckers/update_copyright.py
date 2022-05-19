e07de34b0339bc5a3e9f41d3a542487f19549a4d
David Svoboda <svoboda@cert.org>
2022-05-19 16:09:49 -0400

    Extra files that are hard to legally approve

--- rosecheckers/update_copyright.py
+++ rosecheckers/update_copyright.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-# <legal>CERT Rosecheckers# 
-# Copyright 2022 Carnegie Mellon University.# 
-# NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN 'AS-IS' BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.# 
-# Released under a BSD (SEI)-style license, please see license.txt or contact permission@sei.cmu.edu for full terms.# 
-# [DISTRIBUTION STATEMENT A] This material has been approved for public release and unlimited distribution.  Please see Copyright notice for non-US Government use and distribution.# 
-# CERT® is registered in the U.S. Patent and Trademark Office by Carnegie Mellon University.# 
-# This Software includes and/or makes use of Third-Party Software subject to its own license.# 
-# DM21-0505</legal># 
+# <legal>CERT Rosecheckers#
+# Copyright 2022 Carnegie Mellon University.#
+# NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN 'AS-IS' BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.#
+# Released under a BSD (SEI)-style license, please see license.txt or contact permission@sei.cmu.edu for full terms.#
+# [DISTRIBUTION STATEMENT A] This material has been approved for public release and unlimited distribution.  Please see Copyright notice for non-US Government use and distribution.#
+# CERT® is registered in the U.S. Patent and Trademark Office by Carnegie Mellon University.#
+# This Software includes and/or makes use of Third-Party Software subject to its own license.#
+# DM21-0505</legal>#
 
 import fnmatch
 import json
@@ -26,7 +26,6 @@ COPYRIGHT_BLACKLIST = [
     "./.ccls-cache",
     "./.project",
     "./.cproject",
-    "./graphs",
 ]
 COPYRIGHT_FILENAME_MAP = {
     ".gitignore": r'# \1',
